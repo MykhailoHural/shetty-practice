39c52c337d6d76750275a7ff77c5969bcb1dc186
Mykhailo Hural <mykhailo_hural@epam.com>
2026-01-13 00:20:34 +0200

    feat: complete radio button component and validation tests

--- src/pages/component/radioButton.page.ts
+++ src/pages/component/radioButton.page.ts
@@ -1,9 +1,33 @@
 import { Locator, Page } from "@playwright/test";
 
 export class RadioButtonComponent {
-  readonly RadioButtonSection: Locator;
+  readonly radioButtonSection: Locator;
+  readonly radioButtonTitle: Locator;
+  readonly radioButtons: Locator;
 
   constructor(page: Page) {
-    this.RadioButtonSection = page.locator("#radio-btn-example");
+    this.radioButtonSection = page.locator("#radio-btn-example");
+    this.radioButtonTitle = this.radioButtonSection.getByText(
+      "Radio Button Example"
+    );
+    this.radioButtons = this.radioButtonSection.locator(
+      "input[type = 'radio']"
+    );
+  }
+
+  async selectRadioButtonByLabel(label: string) {
+    const button = this.radioButtonSection
+      .locator("label")
+      .filter({ hasText: label })
+      .locator("input");
+
+    await button.check();
+    return button;
+  }
+
+  async selectRadioButtonByIndex(index: number) {
+    const button = this.radioButtons.nth(index);
+    await button.check();
+    return button;
   }
 }
