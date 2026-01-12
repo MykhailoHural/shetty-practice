606f9f10bb8a22653c565582f9cf6b801e1b0917
Mykhailo Hural <mykhailo_hural@epam.com>
2026-01-13 00:57:47 +0200

    fix: chaged spaces

--- src/tests/radioButton.spec.ts
+++ src/tests/radioButton.spec.ts
@@ -2,17 +2,18 @@ import { test, expect } from "@playwright/test";
 import { PracticePage } from "../pages/practice.page";
 
 test.describe("Validation of the radio buttons", () => {
-
-  test('All radio buttons are uchecked', async({page})=>{
+  test("All radio buttons are uchecked", async ({ page }) => {
     const practicePage = new PracticePage(page);
 
     await practicePage.open();
 
-    const allRadioButtons = await practicePage.radioButtonComponent.radioButtons.all();
-    for(const radio of allRadioButtons){
-      await expect(radio).not.toBeChecked()
+    const allRadioButtons =
+      await practicePage.radioButtonComponent.radioButtons.all();
+    for (const radio of allRadioButtons) {
+      await expect(radio).not.toBeChecked();
     }
-  })
+  });
+
   test("Select radio button by index", async ({ page }) => {
     const practicePage = new PracticePage(page);
 
