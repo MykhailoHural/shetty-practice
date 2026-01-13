5b527f4d8dd15643b0543c0fe60d84be2f7375d1
Mykhailo Hural <mykhailo_hural@epam.com>
2026-01-13 16:53:37 +0200

    feature: added check box component and the tests for it

--- src/pages/practice.page.ts
+++ src/pages/practice.page.ts
@@ -1,12 +1,15 @@
 import { BasePage } from "./base.page";
 import { RadioButtonComponent } from "./component/radioButton.page";
+import { CheckBoxComponent } from "./component/checkBox.page";
 import { Page } from "@playwright/test";
 
 export class PracticePage extends BasePage {
   readonly radioButtonComponent: RadioButtonComponent;
+  readonly checkBoxComponent : CheckBoxComponent;
   constructor(page: Page) {
     super(page);
     this.radioButtonComponent = new RadioButtonComponent(page);
+    this.checkBoxComponent = new CheckBoxComponent(page)
   }
 
   async open(){
